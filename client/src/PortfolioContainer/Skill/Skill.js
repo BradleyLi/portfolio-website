1d2ed0f61498b68b7fb01b4d2a59e318dbd6e6da
Bradley Li <jizhe.li@ucalgary.ca>
2023-09-01 21:49:48 -0600

    created the skill component

---
+++ client/src/PortfolioContainer/Skill/Skill.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Skill.css";
+
+export default function Skill(props) {
+  return (
+    <div className="skill-box">
+      <div className="skill-title">{props.title}</div>
+      <div className="skill-icon">{props.icon}</div>
+    </div>
+  );
+}
